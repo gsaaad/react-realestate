d3eb1b11571478349dd82e25e28461ea87b72720
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-24 08:55:26 -0500

    displaying houses for sale, showing price, address sqft bathroom and bedrooms

--- src/components/House/House.js
+++ src/components/House/House.js
@@ -3,25 +3,42 @@ import "./House.css";
 function House(props) {
   var house = props.house;
   return (
-    <div className="row  bg-gradient house">
-      <div className="col text-right">
+    <div className="row  bg-gradient house mt-4">
+      <div className="col text-right mt-4 text-light">
         <img
           src={house.imgSrc}
           alt={house.address}
-          width={document.body.clientWidth * 0.4}
+          width={
+            document.body.clientWidth > 600
+              ? document.body.clientWidth * 0.24
+              : document.body.clientWidth * 0.4
+          }
         />
-        <h2 className="house-property">
-          Listing Status: {house.listingStatus}
+        <h2 className="house-property fs-5">
+          <span className="text-warning">Status: </span>
+          {house.listingStatus.split("_").join(" ")}
         </h2>
-        <h3 className="house-price">Price: ${house.price}</h3>
+        <h3 className="house-price fs-5">
+          <span className="text-warning">Price: $ </span>
+
+          {house.price}
+        </h3>
       </div>
-      <div className="col">
-        <div className="house-bb-rooms bg-secondary rounded-pill p-2 text-center text-light fw-bolder">
-          <h2 className="house-rooms">Bathrooms:{house.bathrooms}</h2>
-          <h2 className="house-rooms">Bedrooms:{house.bedrooms}</h2>
+      <div className="col mt-4">
+        <div className="house-bb-rooms bg-secondary rounded-pill p-2 text-center text-light">
+          <h2 className="house-rooms">Bathrooms: #{house.bathrooms}</h2>
+          <h2 className="house-rooms">Bedrooms: #{house.bedrooms}</h2>
         </div>
-        <h2>House Address: {house.address}</h2>
-        <h3>Price: ${house.price}</h3>
+        <h2 className="fs-6 text-light m-2">
+          <span className="text-warning">Address: </span>
+          {house.address}
+        </h2>
+        <h3 className="fs-6 text-light m-2">
+          {Math.floor(house.lotAreaValue) === 0
+            ? "N/A"
+            : Math.floor(house.lotAreaValue)}
+          <span className="text-warning"> sqft</span>
+        </h3>
       </div>
     </div>
   );
