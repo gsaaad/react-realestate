4d752e5f54affcf1f6216a5e53dd7380a29df1c4
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-29 07:40:01 -0500

    addition of more houses from top cities in US/CA and some style updates

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import HousesForSale from "./components/HousesForSale/HousesForSale";
 import Clock from "./components/Clock/Clock";
-import SearchHouses from "./components/SearchHouses/SearchHouses";
 import SubNav from "./components/SubNav/SubNav";
+import SearchHouses from "./components/SearchHouses/SearchHouses";
 function App() {
   return (
     <div className="App ">
       <Nav />
       <SubNav />
-
-      {/* <SearchHouses /> */}
-      {/* <Clock /> */}
-      {/* <HousesForSale /> */}
+      <SearchHouses />
+      <Clock />
+      <HousesForSale />
     </div>
   );
 }
