922e77cc5c598a17be29477c92df7ff3d36cbe61
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-30 15:15:18 -0500

    added home blog and few article cards representing articles for users to read

--- src/components/HomeBlog/HomeBlog.js
+++ src/components/HomeBlog/HomeBlog.js
@@ -1,15 +1,253 @@
 import "./HomeBlog.css";
 const HomeBlog = () => {
   return (
-    <div className="bg-light mx-auto my-3 p-2 border bg-gradient rounded-pill half-width">
+    <div className="bg-secondary mx-auto my-3 p-2 border bg-gradient rounded half-width">
       <div>
-        <h2>Home Sweet Home Blog</h2>
+        <h2 className="border-bottom">Home Sweet Home Blog</h2>
       </div>
-      <div>
-        <article className="bg-secondary rounded-pill half-width my-2">
-          <h3 className="my-2">Article Heading</h3>
-          <section className="my-2">Article Body</section>
-        </article>
+      <div className="card my-2">
+        <a href="/">
+          <section className="article-main">
+            <article className="half-width my-2">
+              <h3 className="my-2">Article Heading</h3>
+              <img
+                src={process.env.REACT_APP_HOME_ONE}
+                style={{ width: "95%", borderRadius: "10px" }}
+                alt="home real estate sweet home blog article"
+              />
+            </article>
+
+            <section className="my-2 border p-3 article-body">
+              Article Body Lorem ipsum dolor sit ametLorem ipsum dolor sit
+              ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet
+            </section>
+          </section>
+        </a>
+      </div>
+      <div className="card my-2">
+        <a href="/">
+          <section className="article-main">
+            <article className="half-width my-2">
+              <h3 className="my-2">Article Heading</h3>
+              <img
+                src={process.env.REACT_APP_HOME_ONE}
+                style={{ width: "95%", borderRadius: "10px" }}
+                alt="home real estate sweet home blog article"
+              />
+            </article>
+
+            <section className="my-2 border p-3 article-body">
+              Article Body Lorem ipsum dolor sit ametLorem ipsum dolor sit
+              ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet
+            </section>
+          </section>
+        </a>
+      </div>{" "}
+      <div className="card my-2">
+        <a href="/">
+          <section className="article-main">
+            <article className="half-width my-2">
+              <h3 className="my-2">Article Heading</h3>
+              <img
+                src={process.env.REACT_APP_HOME_ONE}
+                style={{ width: "95%", borderRadius: "10px" }}
+                alt="home real estate sweet home blog article"
+              />
+            </article>
+
+            <section className="my-2 border p-3 article-body">
+              Article Body Lorem ipsum dolor sit ametLorem ipsum dolor sit
+              ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet
+            </section>
+          </section>
+        </a>
+      </div>{" "}
+      <div className="card my-2">
+        <a href="/">
+          <section className="article-main">
+            <article className="half-width my-2">
+              <h3 className="my-2">Article Heading</h3>
+              <img
+                src={process.env.REACT_APP_HOME_ONE}
+                style={{ width: "95%", borderRadius: "10px" }}
+                alt="home real estate sweet home blog article"
+              />
+            </article>
+
+            <section className="my-2 border p-3 article-body">
+              Article Body Lorem ipsum dolor sit ametLorem ipsum dolor sit
+              ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
+              ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor
+              sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet
+            </section>
+          </section>
+        </a>
       </div>
     </div>
   );
