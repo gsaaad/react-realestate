966fe72f0d8844a9f8a6dc434b7500dfc6c74246
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-01-11 08:12:59 -0500

    login modal structure complete and responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,7 @@ import "./Login.css";
 const Login = () => {
   return (
     <div className="rounded border-end border-start border-light login-container">
-      <h1 className="bg-light pt-4 text-dark fw-semibold ">Sign In</h1>
+      <h1 className="bg-light pt-4 text-dark fw-semibold">Sign In</h1>
       <div
         style={{
           backgroundImage: `url(${photo_3})`,
@@ -21,6 +21,24 @@ const Login = () => {
         <div className="login-modal-container">
           <h3 className="login-header">Sign in for Sweet Home</h3>
           <form className="login-form">
+            {/* <label htmlFor="user-firstname">First Name</label>
+
+            <input
+              className="form-input"
+              placeholder="First Name"
+              type="text"
+              id="user-firstname"
+            />
+            <label htmlFor="user-lastname">Last Name</label>
+
+            <input
+              className="form-input"
+              placeholder="Last Name"
+              type="text"
+              id="user-lastname"
+            /> */}
+            <label htmlFor="user-email">Email</label>
+
             <input
               className="form-input"
               placeholder="Email"
@@ -28,6 +46,7 @@ const Login = () => {
               type="email"
               id="user-email"
             />
+            <label htmlFor="user-password">Password</label>
             <input
               className="form-input"
               placeholder="********"
@@ -35,7 +54,18 @@ const Login = () => {
               type="password"
               id="user-password"
             />
-            <button className="bg-primary rounded p-2" type="submit">
+            {/* <label htmlFor="user-repassword">Re-enter Password</label>
+            <input
+              className="form-input"
+              placeholder="********"
+              name="password"
+              type="password"
+              id="user-repassword"
+            /> */}
+            <button
+              className="bg-primary rounded p-2 text-light fw-semibold m-2"
+              type="submit"
+            >
               Submit
             </button>
           </form>
