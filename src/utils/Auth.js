0e074e9ed09842ec1923c049318efc769ad9907c
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-01-13 14:29:38 -0500

    addition of authentication tool (JWT) for logged in and session duration

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -32,6 +32,27 @@ class AuthenticationTool {
       }
     }
   }
+  login(token) {
+    localStorage.setItem("id_token", token);
+    window.location.assign("/");
+  }
+  logout() {
+    localStorage.removeItem("id_token");
+    window.location.assign("/");
+  }
+  getToken() {
+    return localStorage.getItem("id_token");
+  }
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      const isExpired = decoded.exp < Date.now() / 1000 ? true : false;
+      return isExpired;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
 }
 
 export default new AuthenticationTool();
