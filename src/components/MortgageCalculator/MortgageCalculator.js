3631b16c3b1a8c936a1c4101bbffc2501ebc62c4
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-01-20 04:22:07 -0500

    addition of inputs for mortgage caclulator to give user a detailed view of payments

--- src/components/MortgageCalculator/MortgageCalculator.js
+++ src/components/MortgageCalculator/MortgageCalculator.js
@@ -16,7 +16,7 @@ const MortgageCalculator = () => {
         </div>
         <div className="bg-light form-calc-container">
           <form className="form-calc">
-            <label className="fs-5">Asking Price:</label>
+            <label>Asking Price:</label>
             <input
               type="text"
               name="askingPrice"
@@ -26,6 +26,63 @@ const MortgageCalculator = () => {
             />
             <button className="calc-btn bg-primary text-light">Go</button>
           </form>
+          <div className="container mortgage-calc">
+            <div className="row">
+              <div className="col"></div>
+              <div className="col">Scenario 1</div>
+              <div className="col">Scenario 2</div>
+              <div className="col">Scenario 3</div>
+            </div>
+            <div className="row border-bottom border-2">
+              <div className="col">Down Payment</div>
+              <div className="col">5%</div>
+              <div className="col">10%</div>
+              <div className="col">15%</div>
+            </div>
+            <div className="row border-bottom border-2">
+              <div className="col">Amount:</div>
+              <div className="col">50,000</div>
+              <div className="col">100,000</div>
+              <div className="col">150,000</div>
+            </div>
+            <div className="row border-bottom border-2">
+              <div className="col">Insurance:</div>
+              <div className="col">20,000</div>
+              <div className="col">15,000</div>
+              <div className="col">9,000</div>
+            </div>
+            <div className="row border-bottom border-2 bg-primary text-light">
+              <div className="col">TOTAL Mortage Required</div>
+              <div className="col">50,000</div>
+              <div className="col">100,000</div>
+              <div className="col">150,000</div>
+            </div>
+            <div className="row">
+              <div className="col"></div>
+              <div className="col">Scenario 1</div>
+              <div className="col">Scenario 2</div>
+              <div className="col">Scenario 3</div>
+            </div>
+            <div className="row border-bottom border-2">
+              <div className="col">Amortization period</div>
+              <div className="col">
+                <span>5 Year </span>
+              </div>
+              <div className="col">
+                <span>15 Year </span>
+              </div>
+              <div className="col">
+                <span>25 Year </span>
+              </div>
+            </div>
+
+            <div className="row border-bottom border-2">
+              <div className="col">Mortgage Rate</div>
+              <div className="col">50,000</div>
+              <div className="col">100,000</div>
+              <div className="col">150,000</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
