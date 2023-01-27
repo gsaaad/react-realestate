fb5de5eb9d2882ad927ec52d9f6c8d48ae187711
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-01-27 18:57:06 -0500

    addition of seed script. npm run seed to insert 50 houses into mongoDB noSQL database

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,45 +1,23 @@
 const HouseData = require("../houses.json");
 const dbConnection = require("../config/connection");
-const mongoose = require("mongoose");
-require("dotenv").config();
 
-var MONGODB_PASS = process.env.mongoDB;
-
-const collection = "Single_Homes";
-
-// dbConnection.once("open", () => {
-//   console.log("Connection to DB..Inserting")
-//   dbConnection
-//     .useDb("properties")
-//     .collection(collection)
-//     .insertMany(HouseData.props);
-// });
-// dbConnection.close();
-
-mongoose.set("strictQuery", true);
-
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    `mongodb+srv://gsaad:${MONGODB_PASS}@cluster0.h2arlfo.mongodb.net/?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-mongoose.connection.on("open", () => {
-  console.log("Connection to DB..Inserting");
-  //   dbConnection
-  //     .useDb("properties")
-  //     .collection(collection)
-  //     .insertMany(HouseData.props);
-  // });
-  mongoose.connection
+dbConnection.on("open", async () => {
+  const getRandomHouses = () => {
+    var listOfHouses = [];
+    for (let i = 0; i < 3; i++) {
+      var randomNum = Math.floor(Math.random() * HouseData.props.length - 1);
+      var house = HouseData.props[randomNum];
+      listOfHouses.push(house);
+    }
+    console.log({ listOfHouses });
+    return listOfHouses;
+  };
+  var ArrayHouses = getRandomHouses();
+  await dbConnection
     .useDb("properties")
-    .collection(collection)
-    .insertMany(HouseData.props);
+    .collection("Single_Homes")
+    .insertMany(ArrayHouses);
+
+  console.log("All Done Seeding.. Enjoy your search for a Home Sweet Home!");
+  process.exit(0);
 });
-
-console.log("all Done seeding...");
-
-// mongoose.connection.close();
