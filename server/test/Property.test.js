abb9e7b33936c6a1db210a9110551220b57e9d81
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-01-31 18:50:53 -0500

    property tests for posting a house and required methods

--- server/test/Property.test.js
+++ server/test/Property.test.js
@@ -12,111 +12,29 @@ chai.use(chaiHttp);
 
 var requester = chai.request(server);
 
+describe("Properties", () => {
+  beforeEach((done) => {
+    Property.deleteMany({}, (err) => {
+      console.error(err);
+      done();
+    });
+  });
+});
+
 // test object and values
 describe("Creates a Property Object", () => {
   it("Should create a property object", () => {
-    var propertyType = "SINGLE_HOME";
-    var country = "CAN";
-    var imgSrc =
-      "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
-    var status = "FOR_SALE";
-
-    const property = new Property({
-      propertyType: propertyType,
-      country: country,
-      imgSrc: imgSrc,
-      status: status,
-    });
-
+    const property = new Property();
     assert.typeOf(property, "Object", "Property input is not an Object");
-  });
-  it("Should have valid country entry", () => {
-    var propertyType = "SINGLE_HOME";
-    var country = "CAN";
-    var imgSrc =
-      "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
-    var status = "FOR_SALE";
-
-    const property = new Property({
-      propertyType: propertyType,
-      country: country,
-      imgSrc: imgSrc,
-      status: status,
-    });
-
-    assert.equal(
-      property.country,
-      "CAN",
-      `Property country does not match ${country}`
-    );
-  });
-  it("Should have valid property Type", () => {
-    var propertyType = "SINGLE_HOME";
-    var country = "CAN";
-    var imgSrc =
-      "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
-    var status = "FOR_SALE";
-
-    const property = new Property({
-      propertyType: propertyType,
-      country: country,
-      imgSrc: imgSrc,
-      status: status,
-    });
-
-    assert.equal(
-      property.propertyType,
-      propertyType,
-      `Property property does not match ${propertyType}`
-    );
-  });
-  it("Should have a valid img Photo", () => {
-    var propertyType = "SINGLE_HOME";
-    var country = "CAN";
-    var imgSrc =
-      "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
-    var status = "FOR_SALE";
-
-    const property = new Property({
-      propertyType: propertyType,
-      country: country,
-      imgSrc: imgSrc,
-      status: status,
-    });
-
-    assert.equal(
-      property.imgSrc,
-      imgSrc,
-      `Property image does not match ${imgSrc}`
-    );
-  });
-  it("Should have valid property status", () => {
-    var propertyType = "SINGLE_HOME";
-    var country = "CAN";
-    var imgSrc =
-      "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
-    var status = "FOR_SALE";
-
-    const property = new Property({
-      propertyType: propertyType,
-      country: country,
-      imgSrc: imgSrc,
-      status: status,
-    });
-
-    assert.equal(
-      property.status,
-      status,
-      `Property status does not match ${status}`
-    );
   });
 });
 
 // Testing routes
 
-describe("/GET Properties", () => {
+describe("GET All Properties", () => {
   it("should get all properties", (done) => {
     requester.get("/api/property").end((err, res) => {
+      console.log(res);
       expect(res.status).to.equal(200);
       expect(res.body).to.be.an("array");
       expect(res.body.length).to.be.at.most(7);
@@ -125,13 +43,74 @@ describe("/GET Properties", () => {
   });
 });
 
-describe("/GET Properties", () => {
-  it("should get all properties", (done) => {
-    requester.get("/api/property").end((err, res) => {
-      expect(res.status).to.equal(200);
-      expect(res.body).to.be.an("array");
-      expect(res.body.length).to.be.at.most(7);
-      done();
+describe("/POST Properties", () => {
+  it("Should not POST a property without propertyType", (done) => {
+    var propertyType = "SINGLE_HOME";
+    const property = new Property({
+      propertyType: propertyType,
+    });
+    requester.post(property).end((error, res) => {
+      console.log(res);
     });
   });
+  //   it("Should have valid property Type", () => {
+  //     var propertyType = "SINGLE_HOME";
+  //     var country = "CAN";
+  //     var imgSrc =
+  //       "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
+  //     var status = "FOR_SALE";
+
+  //     const property = new Property({
+  //       propertyType: propertyType,
+  //       country: country,
+  //       imgSrc: imgSrc,
+  //       status: status,
+  //     });
+
+  //     assert.equal(
+  //       property.propertyType,
+  //       propertyType,
+  //       `Property property does not match ${propertyType}`
+  //     );
+  //   });
+  //   it("Should have a valid img Photo", () => {
+  //     var propertyType = "SINGLE_HOME";
+  //     var country = "CAN";
+  //     var imgSrc =
+  //       "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
+  //     var status = "FOR_SALE";
+
+  //     const property = new Property({
+  //       propertyType: propertyType,
+  //       country: country,
+  //       imgSrc: imgSrc,
+  //       status: status,
+  //     });
+
+  //     assert.equal(
+  //       property.imgSrc,
+  //       imgSrc,
+  //       `Property image does not match ${imgSrc}`
+  //     );
+  //   });
+  //   it("Should have valid property status", () => {
+  //     var propertyType = "SINGLE_HOME";
+  //     var country = "CAN";
+  //     var imgSrc =
+  //       "https://photos.zillowstatic.com/fp/019c5f0264378e71bd1807bd36374536-p_e.jpg";
+  //     var status = "FOR_SALE";
+
+  //     const property = new Property({
+  //       propertyType: propertyType,
+  //       country: country,
+  //       imgSrc: imgSrc,
+  //       status: status,
+  //     });
+
+  //     assert.equal(
+  //       property.status,
+  //       status,
+  //       `Property status does not match ${status}`
+  //     );
+  //   });
 });
