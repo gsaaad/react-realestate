01a20f75e858f1e19a2486e4f231edce5c34699d
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-02-04 07:50:52 -0500

    adding agents routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const propertyRoutes = require("./property-routes");
 
 router.use("/property", propertyRoutes);
+router.use("/agent", agentRoutes);
 
 module.exports = router;
