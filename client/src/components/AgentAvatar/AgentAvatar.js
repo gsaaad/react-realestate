be503d1a843c265807889534aa9bc61840da3cbd
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-03-03 15:31:29 -0500

    addition of agent avatar and displaying agents information

--- client/src/components/AgentAvatar/AgentAvatar.js
+++ client/src/components/AgentAvatar/AgentAvatar.js
@@ -5,6 +5,10 @@ function AgentAvatar(props) {
 
   return (
     <div className="col text-right mt-4 text-dark">
+      <h4 className="text-light ">licenseId : {agentAvatar.licenseId}</h4>
+      <h4 className="text-light ">
+        Agent Since : {agentAvatar.agentSince.slice(0, 10)}
+      </h4>
       <img
         src={agentAvatar.imgSrc}
         alt={agentAvatar.email}
@@ -17,22 +21,12 @@ function AgentAvatar(props) {
       />
       <div className="bg-gradient rounded border border-secondary m-2 p-2 shadow-lg">
         <h2 className="m-2 text-warning">
-          <span className="border-bottom border-dark text-light m-2">
-            Agent Name:
-          </span>
+          <span className=" border-dark text-light m-2">Agent Name:</span>
           {agentAvatar.name}
         </h2>
         <h2 className="m-2 text-warning">
-          <span className="border-bottom border-dark text-light m-2">
-            Located:
-          </span>
-          {agentAvatar.city}
-        </h2>
-        <h2 className="m-2 text-warning">
-          <span className="border-bottom border-dark text-light m-2">
-            Professional Statement
-          </span>
-          {agentAvatar.profileStatement}
+          <span className=" border-dark text-light m-2">Represents:</span>
+          {agentAvatar.representsRealtor}
         </h2>
       </div>
     </div>
--- client/src/components/Agents/Agents.js
+++ client/src/components/Agents/Agents.js
@@ -7,7 +7,17 @@ function Agent(props) {
   return (
     <div className="row bg-gradient mt-4 rounded">
       <AgentAvatar agent={agent} />
-      <div className="rounded-pill p-2 text-center text-light"></div>
+      <div className="rounded-pill p-2 text-center text-light">
+        <h2 className="m-2 text-warning">
+          <span className=" border-dark text-light m-2">Located:</span>
+          {agent.city}
+        </h2>
+
+        <h2 className="border-bottom border-dark text-light m-2">
+          Professional Statement:
+        </h2>
+        <h2 className="m-2 text-warning">{agent.profileStatement}</h2>
+      </div>
     </div>
   );
 }
