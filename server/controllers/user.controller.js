560296d1b4824f031382fe9c097805b88a6a8eae
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-03-10 19:51:27 -0500

    setting up user controller for querying through users by ID, first name and last name

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,6 +12,52 @@ const userController = {
       })
       .catch((e) => {
         console.error(e, this.errorMessage);
+        res.sendStatus(400).json({ message: this.errorMessage });
+      });
+  },
+  getUserById({ params }, res) {
+    User.findOne({ _id: [params.id] })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: this.idMessage });
+          return false;
+        }
+        res.json(agentData);
+      })
+      .catch((e) => {
+        console.error(e, this.errorMessage);
+        res.sendStatus(400).json({ message: this.errorMessage });
+      });
+  },
+  getUserByFistName({ params }, res) {
+    const firstName = params.firstName;
+    console.log("Params first name for User" + firstName);
+    User.find({ firstName: firstName })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: this.errorMessage });
+        }
+        res.json(userData);
+      })
+      .catch((e) => {
+        console.error(e, this.errorMessage);
+        res.sendStatus(400).json({ message: this.errorMessage });
+      });
+  },
+  getUserByLastName({ params }, res) {
+    const lastName = params.lastName;
+    console.log("Params last Name for User" + lastName);
+
+    User.find({ lastName: lastName })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: this.errorMessage });
+        }
+        res.json(userData);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.sendStatus(400).json({ message: this.errorMessage });
       });
   },
 };
